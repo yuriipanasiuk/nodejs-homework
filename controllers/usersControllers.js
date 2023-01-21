db81507efaa722acd325cca8a2df9674f0b9fba1
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-01-21 23:41:01 +0200

    add avatar after register

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,24 +1,32 @@
 const jwt = require('jsonwebtoken');
 const bscrypt = require('bcryptjs');
+const gravatar = require('gravatar');
+const path = require('path');
+const fs = require('fs').promises;
 
 const User = require('../models/user');
 const MyError = require('../helpers/myErrors');
 
 const { SECRET_KEY } = process.env;
 
-const registerUser = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
+const storageImage = path.join(process.cwd(), 'public', 'avatars');
 
+const registerUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
     const user = await User.findOne({ email });
 
     if (user) {
       return next(new MyError('Email in use', 409));
     }
 
+    const avatarURL = gravatar.url(email);
+
     await User.create({
       email,
       password,
+      avatarURL,
     });
 
     const { subscription } = await User.findOne({ email });
@@ -27,6 +35,7 @@ const registerUser = async (req, res, next) => {
       user: {
         email,
         subscription,
+        avatarURL,
       },
     });
   } catch (error) {
@@ -35,8 +44,9 @@ const registerUser = async (req, res, next) => {
 };
 
 const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
   try {
-    const { email, password } = req.body;
     const user = await User.findOne({ email });
 
     if (!user) {
@@ -71,9 +81,9 @@ const loginUser = async (req, res, next) => {
 };
 
 const logoutUser = async (req, res, next) => {
-  try {
-    const { _id } = req.user;
+  const { _id } = req.user;
 
+  try {
     await User.findByIdAndUpdate(_id, { token: null });
 
     res.status(204).json();
@@ -96,14 +106,33 @@ const getCurrentUser = async (req, res, next) => {
 };
 
 const updateUserSubscription = async (req, res, next) => {
-  try {
-    const { _id } = req.user;
+  const { _id } = req.user;
 
+  try {
     const result = await User.findByIdAndUpdate(_id, req.body, { new: true });
     const { email, subscription } = result;
 
     res.json({ email, subscription });
   } catch (error) {
+    next(error);
+  }
+};
+
+const updateAvatar = async (req, res, next) => {
+  const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+
+  try {
+    const resultUpload = path.join(storageImage, `${id}_${originalname}`);
+
+    fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join('public', 'avatar', originalname);
+
+    await User.findByIdAndUpdate(req.user._id, { avatarURL });
+
+    res.json({ avatarURL });
+  } catch (error) {
+    fs.unlink(tempUpload);
     next(error);
   }
 };
@@ -114,4 +143,7 @@ module.exports = {
   logoutUser,
   getCurrentUser,
   updateUserSubscription,
+  updateAvatar,
 };
+
+//TODO: resize avatar by using jimp
